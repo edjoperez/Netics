956a1aad3dfb9cfc3c3ca8c5ef9cdcb5ea47a0c9
Ed <edjo.perez@gmail.com>
2012-10-09 00:00:54 -0500

    Forgot Second Pane added

--- HTML/js/main.js
+++ HTML/js/main.js
@@ -2,4 +2,5 @@ $(function()
 	{
 		//$("a#signIn").colorbox({href:"login",title:"login",width:"360px",height:"185px"});
 		$("a#signIn").colorbox();
+		$("a.lastStepForgot").colorbox({inline:true,href:"#forgotMessageContainer"})
 	});
