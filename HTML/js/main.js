39e5256251399d74444ed01b5233f324d49a1816
Ed <edjo.perez@gmail.com>
2012-10-15 15:07:02 -0500

    Basic Effects applied

--- HTML/js/main.js
+++ HTML/js/main.js
@@ -2,5 +2,18 @@ $(function()
 	{
 		//$("a#signIn").colorbox({href:"login",title:"login",width:"360px",height:"185px"});
 		$("a#signIn").colorbox();
-		$("a.lastStepForgot").colorbox({inline:true,href:"#forgotMessageContainer"})
+		$("a.lastStepForgot").colorbox({inline:true,href:"#forgotMessageContainer"});
+		$("a.lastStepSignUp").colorbox({inline:true,href:"#signUpMessageContainer"});
+
+		console.log($("div.page"));
+		$("a.button")
+		var i = 0;
+		$("div#signUpContent button.next").click(function()
+			{
+				console.log("Hola");
+				i++;
+				$(this).parents("div.page").toggle();
+				$($("div#signUpContent div.page")[i]).toggle();
+			});
+		
 	});
