32bc6c3a370d45c866f0029fdc40d1c643828fe1
Ed <edjo.perez@gmail.com>
2012-10-15 16:37:05 -0500

    Forgot - Basic Effects applied

--- HTML/js/main.js
+++ HTML/js/main.js
@@ -2,18 +2,14 @@ $(function()
 	{
 		//$("a#signIn").colorbox({href:"login",title:"login",width:"360px",height:"185px"});
 		$("a#signIn").colorbox();
-		$("a.lastStepForgot").colorbox({inline:true,href:"#forgotMessageContainer"});
-		$("a.lastStepSignUp").colorbox({inline:true,href:"#signUpMessageContainer"});
+		$("a#lastStep").colorbox({inline:true,href:"#messageContainer"});
 
-		console.log($("div.page"));
-		$("a.button")
 		var i = 0;
-		$("div#signUpContent button.next").click(function()
+		$("button.next").click(function()
 			{
-				console.log("Hola");
 				i++;
 				$(this).parents("div.page").toggle();
-				$($("div#signUpContent div.page")[i]).toggle();
+				$($("div.page")[i]).toggle();
 			});
 		
 	});
