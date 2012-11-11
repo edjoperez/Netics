316256502e72dc91d18d8ac4a0b8ed99a7c70200
Ed <edjo.perez@gmail.com>
2012-11-10 19:58:57 -0500

    Index: Login was changed to log with facebook

---
+++ BK/js/main.js
@@ -0,0 +1,15 @@
+$(function()
+	{
+		//$("a#signIn").colorbox({href:"login",title:"login",width:"360px",height:"185px"});
+		$("a#signIn").colorbox();
+		$("a#lastStep").colorbox({inline:true,href:"#messageContainer"});
+
+		var i = 0;
+		$("button.next").click(function()
+			{
+				i++;
+				$(this).parents("div.page").toggle();
+				$($("div.page")[i]).toggle();
+			});
+		
+	});
